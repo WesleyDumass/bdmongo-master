78cee5778493efc24b01a02a5b56ed91f04a2d59
unknown <wesley.dumas12@gmail.com>
2024-11-29 13:18:57 -0300

    forte

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
-const setupSwagger = require('../swagger/swagger')
+
 const app = express();
 
 
 app.use(express.json());
-setupSwagger(app);
 
-app.use('/api', routes);
+app.use(routes);
 
 module.exports = app;
